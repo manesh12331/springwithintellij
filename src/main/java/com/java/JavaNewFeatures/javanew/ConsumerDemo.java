129a73b9a26bae55d5b9c6b3eda6155d914e84ab
manesh12331 <maneshpunde69@gmail.com>
2021-07-17 16:47:23 +0530

    Add new java file

--- src/main/java/com/java/JavaNewFeatures/javanew/ConsumerDemo.java
+++ src/main/java/com/java/JavaNewFeatures/javanew/ConsumerDemo.java
@@ -6,5 +6,6 @@ public class ConsumerDemo {
     public static void main(String[] args) {
         Consumer<Integer> consumer=t -> System.out.println("prinnting:"+t);
         consumer.accept(10);
+        //change in Consumer file
     }
 }
